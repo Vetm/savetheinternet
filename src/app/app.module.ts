c100a90e7746a40531666be77c248a8c94f88bc0
Vetm <mail@jonashohmann.com>
2018-06-13 17:45:28 +0200

    Add what you can do section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { HeaderComponent } from './landing/header/header.component';
 import { Article13Component } from './landing/article13/article13.component';
+import { WhatDoComponent } from './landing/what-do/what-do.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     HeaderComponent,
-    Article13Component
+    Article13Component,
+    WhatDoComponent
   ],
   imports: [
     BrowserModule,
